c6c97fcf1b9b14b0368637c1d94570b5e556ef8a
DmitriiIv <dmitrii@mail333.com>
2019-05-16 11:58:39 +0300

    Migrate to mongo

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 from flask import Flask, request, jsonify
 import json
 from flask_cors import CORS, cross_origin
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config["MONGO_URI"] = os.environ.get('DATABASE_URL')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-db = SQLAlchemy(app)
-
-class WEBM(db.Model):
-    __tablename__ =  'webms'
-    id = db.Column(db.Integer, primary_key=True)
-    md5 = db.Column(db.String(150), unique=True, nullable=False)
-    screamer_chance = db.Column(db.Float, unique=False, nullable=False)
-    
-    def __init__(self, md5=None, screamer_chance=None):
-        self.md5 = md5
-        self.screamer_chance = screamer_chance
+mongo = PyMongo(app)
 
 from detector import get_data    
 
@@ -30,16 +20,15 @@ from detector import get_data
 def hello():
     content = request.get_json()
     webm = None
-    webm = WEBM.query.filter_by(md5=content['md5']).first()
+    webm = mongo.db.webms.find_one({'md5': content['md5']})
     if (webm is None):
         data = get_data(content)
-        webm = WEBM(data['md5'], data['scream_chance'])
-        db.session.add(webm)
-        db.session.commit()
+        webm = {'md5': data['md5'], 'screamer_chance': data['scream_chance']}
+        mongo.db.webms.insert_one(webm)
     else:
         data = {}
-        data["md5"] = webm.md5
-        data["scream_chance"] = webm.screamer_chance
+        data["md5"] = webm['md5']
+        data["scream_chance"] = webm['screamer_chance']
 
     print(jsonify(data))
     return jsonify(data)
